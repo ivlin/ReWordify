6afa8a6af7b5db355c9bf50a54b22fdf31c7551b
ivlin <ivlin112@gmail.com>
2015-12-29 22:43:57 -0500

    Delete reword.js

--- reword.js
+++
@@ -1,9 +0,0 @@
-//this is the javascript for the popup
-
-chrome.browserAction.onClicked.addListener(function(tab){
-    chrome.tabs.executeScript(tab.id, {file:"content.js"});
-    chrome.tabs.sendMessage(tab.id, {}, function(response){
-	document.getElementById("test").innerHTML = response.message;
-    });
-});
-
