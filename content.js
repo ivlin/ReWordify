178299180f79c62e5b822c9c8390d0b47d4c51f1
RJZheng1 <stuykid2016@yahoo.com>
2016-01-03 16:38:56 -0500

    No longer requires special permissions. Popup page injects and communicates with content script instead of having a background page that does that.

--- content.js
+++ content.js
@@ -1,28 +1,37 @@
-/*
-  The content script is injected into every page and is used to interact with the DOM.
-*/
+//The content script is injected into every page and is used to interact with the DOM.
 
-var simplify = function simplify(){
-    document.getElementsByTagName("title")[0].innerHTML = "SIMPLE";
-}
-
-var complicate = function complicate(){
-    document.getElementsByTagName("title")[0].innerHTML = "COMPLEX";
-}
-
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
-    /*
-      Listens for a message from the background page (background.js)
-      Request is the message. Request is a JSON object where mode is a string containing simplify or complicate.
-    */
-    switch(request.mode){
-    case "simplify":
-	simplify();
-	break;
-    case "complicate":
-	complicate();
-	break;
-    default:
-	break;
+(function() {
+    //Prevents the content script from being injected multiple times
+    if(window.injected){
+	return;
     }
-});
+    window.injected = true;
+    
+    var title = document.getElementsByTagName("title")[0].innerHTML;
+    
+    var simplify = function simplify(){
+	document.getElementsByTagName("title")[0].innerHTML = "SIMPLE";
+    }
+
+    var complicate = function complicate(){
+	document.getElementsByTagName("title")[0].innerHTML = "HARD";
+    }
+
+    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+	/*
+	  Listens for a message from popup.js
+	  Request is the message. It can be of any type.
+	  Currently, request is a JSON object where mode is a string containing simplify or complicate.
+	*/
+	switch (request.mode){
+	case "simplify":
+	    simplify();
+	    break;
+	case "complicate":
+	    complicate();
+	    break;
+	default:
+	    break;
+	}
+    });
+})();
--- popup.js
+++ popup.js
@@ -2,14 +2,15 @@
   This is the javascript attached to the popup box that appears when the extension is clicked
 */
 
+chrome.tabs.executeScript(null, {file:"content.js"});
+
 var sendInstructions = function sendInstructions(){
-    if (document.getElementById("simplify").checked){
+    if(document.getElementById("simplify").checked){
 	chrome.tabs.query({active:true, currentWindow:true},function(tab){
 	    chrome.tabs.sendMessage(tab[0].id, {mode:"simplify"}, function(response){
 	    });
 	});
-    }
-    if (document.getElementById("complicate").checked){
+    }else if(document.getElementById("complicate").checked){
 	chrome.tabs.query({active:true, currentWindow:true},function(tab){
 	    chrome.tabs.sendMessage(tab[0].id, {mode:"complicate"}, function(response){
 	    });
@@ -17,4 +18,4 @@ var sendInstructions = function sendInstructions(){
     }
 };
 
-document.getElementById("submit").addEventListener("click",sendInstructions);
+document.getElementById("submit").addEventListener("click", sendInstructions);
