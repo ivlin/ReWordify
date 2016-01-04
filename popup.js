692e9d2f64575334fbe0dae87d067fd4a3af2eda
Ivan Lin <ivlin112@gmail.com>
2016-01-04 12:14:48 -0500

    separated getactivetab

--- popup.js
+++ popup.js
@@ -6,16 +6,22 @@ chrome.tabs.executeScript(null, {file:"content.js"});
 
 var sendInstructions = function sendInstructions(){
     if(document.getElementById("simplify").checked){
-	chrome.tabs.query({active:true, currentWindow:true},function(tab){
-	    chrome.tabs.sendMessage(tab[0].id, {mode:"simplify"}, function(response){
-	    });
+	getActiveTab().then(function(tab){
+	    chrome.tabs.sendMessage(tab[0].id, {mode:"simplify"});
 	});
     }else if(document.getElementById("complicate").checked){
-	chrome.tabs.query({active:true, currentWindow:true},function(tab){
-	    chrome.tabs.sendMessage(tab[0].id, {mode:"complicate"}, function(response){
-	    });
+	getActiveTab().then(function(tab){
+	    chrome.tabs.sendMessage(tab[0].id, {mode:"complicate"});
 	});
     }
 };
+
+var getActiveTab = function getActiveTab(){
+    return new Promise(function(resolve,reject){
+	chrome.tabs.query({active:true,currentWindow:true},function(tab){
+	    resolve(tab);
+	});
+    });
+}
 
 document.getElementById("submit").addEventListener("click", sendInstructions);
