95f012fa2990685f62c14ac5f6ef3873502739fd
Shi Shu <shishu_1998@yahoo.com>
2016-01-05 12:15:41 -0500

    different API

--- static/script.js
+++ static/script.js
@@ -1,12 +1,12 @@
-var api_key = "e4383d803d79b55ef72d1a68e85d075d";
+var api_key = "TNMlDYqLB3SteQle6a9U";
 
 var getsyn = function(word){
     console.log("Input: " + word);
     $.ajax({	
 	type: 'GET',
-	url: "http://words.bighugelabs.com/api/2/" + api_key+"/" + word + "/json?callback=?",
+	url: "http://thesaurus.altervista.org/thesaurus/v1?word="+word +"&language=en_US&key="+ api_key +"&output=json",
 	datatype:'json',
-	sucess: function(data){
+	success: function(data){
 	    console.log(data);
 	}
 	
