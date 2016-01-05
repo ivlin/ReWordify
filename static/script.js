22bd6d92aea405c8f90766f64cbed6438463676d
Shi Shu <shishu_1998@yahoo.com>
2016-01-05 12:20:03 -0500

    stuff

--- static/script.js
+++ static/script.js
@@ -5,10 +5,27 @@ var getsyn = function(word){
     $.ajax({	
 	type: 'GET',
 	url: "http://thesaurus.altervista.org/thesaurus/v1?word="+word +"&language=en_US&key="+ api_key +"&output=json",
-	datatype:'json',
+	datatype:'jsonp',
 	success: function(data){
 	    console.log(data);
 	}
 	
     });
 };
+
+/*
+var api_key = "e4383d803d79b55ef72d1a68e85d075d";
+var getsyn = function(word){
+    console.log("Input: " + word);
+    $.ajax({
+	type= 'GET',
+	url: "http://words.bighugelabs.com/api/2/" + api_key+"/" + word + "/json?callback=?",
+	datatype:'json',
+	type: 'GET',
+	url: "http://words.bighugelabs.com/api/2/" + api_key+"/" + word + "/json?callback=?",
+	datatype:'json',
+	success: function(data){
+	    console.log(data);
+	}
+    });
+};*/
