fa96c1f6786a2088684b2cfd482450dc8fff0250
Shi Shu <shishu_1998@yahoo.com>
2016-01-06 12:06:54 -0500

    returns data

--- static/script.js
+++ static/script.js
@@ -1,9 +1,11 @@
 var api_key = "e4383d803d79b55ef72d1a68e85d075d";
+var synonyms = new Array(50);
+
 
 var getsyn = function(word){
-	console.log("Input: " + word);
-	$.getJSON("http://words.bighugelabs.com/api/2/" + api_key+"/" + word + "/json", function(data){
-		console.log(JSON.stringify(data));
-	});
-}
+    console.log("Input: " + word);
+    $.getJSON("http://words.bighugelabs.com/api/2/" + api_key+"/" + word + "/json", function(data){
+	return data;
+    });
+};
 
