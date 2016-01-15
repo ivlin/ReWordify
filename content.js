469b3a189c406b27169fb8a5e381afbe1f705d49
Shi Shu <shishu_1998@yahoo.com>
2016-01-15 12:16:27 -0500

    reuploaded API stuff

--- content.js
+++ content.js
@@ -6,6 +6,34 @@
 	return;
     }
     window.injected = true;
+    //Thesaurus API stuff
+    var api_key = "e4383d803d79b55ef72d1a68e85d075d";
+    var synonyms = new Array(50);
+    var adjective = new Array(50);
+    var noun = new Array(50);
+    var verb = new Array(50);
+
+    var getsyn = function(word){
+	console.log("Input: " + word);
+	$.getJSON("http://words.bighugelabs.com/api/2/" + api_key+"/" + word + "/json", function(data){
+	    synonyms = data;
+	    if(checkdefined(synonyms.adjective)){
+		adjective = synonyms.adjective.syn;
+	    }
+	    if(checkdefined(synonyms.noun)){
+		noun = synonyms.noun.syn;
+	    }
+	    if(checkdefined(synonyms.verb)){
+		verb = synonyms.verb.syn;
+	    }
+	});
+    };
+
+    var checkdefined = function(array){
+	return array != undefined;
+    };
+    //ajax call to frequency table
+
 
     var freq_list;
     
@@ -85,6 +113,7 @@
     var hardToSimple = function hardToSimple(){
 	/* This will generate a dictionary where keys are the words to be replaced
 	   The value of each key is the word that will replace it*/
+	
 	return {"Sweet":"HAAAAAAAAAAAAAAAAAAA","Just":"well"};
     };
 
